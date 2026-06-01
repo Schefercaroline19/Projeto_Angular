bdf43d1ca884d8cb7bcd9205936c3e91597e6fbd
Schefercaroline19 <20241ctb0100073@estudantes.ifpr.edu.br>
2026-06-01 20:15:54 +0000

    Aula15: Configura ambiente Angular com Material e primeira tela do Portfolio

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('portfolio-angular');
+  protected readonly title = signal('Portfolio Angular');
 }
