f5a91242454345303996a278920a20450fa3364a
Schefercaroline19 <20241ctb0100073@estudantes.ifpr.edu.br>
2026-06-15 20:26:07 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

---
+++ portfolio-angular/src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatCardModule} from '@angular/material/card';
+
+@Component({
+    selector: 'app-home',
+    imports: [MatCardModule],
+    templateUrl: './home.html',
+    styleUrl: './home.css'
+})
+export class Home {}
+
