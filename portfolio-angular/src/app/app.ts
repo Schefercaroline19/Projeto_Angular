a51db6fa3c2852033505a1a732cbefac2cf3caa7
Schefercaroline19 <20241ctb0100073@estudantes.ifpr.edu.br>
2026-08-17 20:34:23 +0000

    Aula18: Angular consumindo a API (Projetos, Catalogo e botao GitHub)

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector:
-  'app-root',
+  selector: 'app-root',
   imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
-  templateUrl:
-  './app.html',
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  title = signal('portfolio-angular');
+}
